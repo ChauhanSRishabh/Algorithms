a7ba4e8714243c438695406d17f6cfa4581aafcb
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-07-06 16:34:37 +0530

    Function named quicksort written

--- Sorting/quick_sort.c
+++ Sorting/quick_sort.c
@@ -17,4 +17,17 @@ void printArray(int arr[], int size)
     printf("\n");
 }
 
+void quicksort(int arr[], int beg, int end)
+{
+    if(beg<end)
+    {
+        int p = partition(arr, beg, end);
+        quicksort(arr, beg, p-1);
+        quicksort(arr, p+1, end);
+    }
+}
 
+int partition(int arr[], int beg, int end)
+{
+    
+}
