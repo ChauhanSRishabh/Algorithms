90e6bebba0a21dfab924ac40a9af8e68b2f599cb
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-07-06 17:25:32 +0530

    Quick Sort implemented Comments improved

--- Sorting/quick_sort.c
+++ Sorting/quick_sort.c
@@ -27,7 +27,7 @@ void quicksort(int arr[], int beg, int end)
     }
 }
 
-// We take last element as pivot
+// This function takes last element as pivot, places the pivot element at its correct position in sorted array, and places all smaller (smaller than pivot) elements to the left of pivot and all greater elements to the right of pivot
 int partition(int arr[], int beg, int end)
 {
     int pivot = arr[end];
@@ -45,6 +45,28 @@ int partition(int arr[], int beg, int end)
     }
     
     // Putting pivot in it's correct position
-    swap(&arr[i + 1], &arr[end]);
+    swap(&arr[i + 1], &arr[end]); // We didn't write pivot in place of arr[end] coz we want to make changes in the array itself
+
     return (i + 1); //returning that index where pivot is placed as quicksort will now be done on the left and right side of pivot
+}
+
+int main()
+{
+    int n;
+    printf("Enter the size of array : ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("\nEnter the array elements : ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("\nGiven array is : ");
+    printArray(arr, n);
+
+    quicksort(arr, 0, n - 1);
+
+    printf("\nSorted array is : ");
+    printArray(arr, n);
+
+    return 0;
 }
