203c4a1d03820c136d463b9b37e25d464a16ab83
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-07-06 17:22:06 +0530

    Partition function written Puts pivot in it's correct position Makes sure the elements left of pivot are smaller than pivot and elements to the right are bigger

--- Sorting/quick_sort.c
+++ Sorting/quick_sort.c
@@ -9,10 +9,10 @@ void swap(int* a, int* b)
     *b = t;
 }
 
-void printArray(int arr[], int size)
+void printArray(int arr[], int n)
 {
     int i;
-    for (i = 0; i < size; i++)
+    for (i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
@@ -27,7 +27,24 @@ void quicksort(int arr[], int beg, int end)
     }
 }
 
+// We take last element as pivot
 int partition(int arr[], int beg, int end)
 {
+    int pivot = arr[end];
+    int i = (beg - 1); // Index of smaller element, -1
+    // i is also the position of pivot found
+ 
+    for (int j = beg; j < end; j++)
+    {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot)
+        {
+            i++; // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
     
+    // Putting pivot in it's correct position
+    swap(&arr[i + 1], &arr[end]);
+    return (i + 1); //returning that index where pivot is placed as quicksort will now be done on the left and right side of pivot
 }
